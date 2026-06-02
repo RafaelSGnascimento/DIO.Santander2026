69a767b2a5ca4a1736f5d0d66a1678651f036850
RafaelSGnascimento <rafaelsgnascimento@gmail.com>
2026-06-02 09:57:02 -0300

    delentando arquivo teste

--- introJava/src/Cadastro.java
+++ introJava/src/Cadastro.java
@@ -10,3 +10,5 @@ public class Cadastro {
         this.sujo = sujo;
     }
 }
+
+
--- introJava/src/Teste.java
+++
@@ -1,6 +0,0 @@
-public class Teste {
-    public static String testetexto() {
-        System.out.println("Bunda de macaco");
-        return "Teste";
-    }
-}
